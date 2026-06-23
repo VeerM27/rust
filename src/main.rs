382e21cf3add58a77e0e8c0e9472a6a440ae0875
Veer Mehta <mehtaveer2711@gmail.com>
2026-06-23 11:02:08 +0530

    option enum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-enum Result<A, B> {
+enum MyResult<A, B> {
     Ok(A),
     Err(B),
 }
@@ -15,5 +15,15 @@ fn main() {
         Err(e) => {
             println!("Error reading file: {}", e);
         }
+    }
+}
+
+fn read_from_file(file_content: String) -> MyResult<String, String> {
+    let res = fs::read_to_string("example.txt");
+
+    if let Ok(content) = res {
+        return MyResult::Ok(content);
+    } else {
+        return MyResult::Err("Failed to read file".to_string());
     }
 }
